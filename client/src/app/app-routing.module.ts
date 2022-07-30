b6ea9f64cc6cb9946234dc51a9ec8a6fa041bf0d
Телицын Никита <dmtvn.labs@yandex.ru>
2022-07-30 23:55:50 +0300

    Added header

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', loadChildren: () => import('./domains/about/about.module').then(md => md.AboutModule)},
+  {path: 'catalog', loadChildren: () => import('./domains/catalog/catalog.module').then(md => md.CatalogModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/domains/catalog/catalog.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  templateUrl: 'catalog.component.html',
+  styleUrls: ['catalog.component.scss'],
+  providers: []
+})
+export class CatalogComponent { }
